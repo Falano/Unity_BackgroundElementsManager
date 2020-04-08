5f58c038937f5c3256e70348734c454be1ea598f
no <noemie.scherer@gmail.com>
2020-04-08 20:37:08 +0200

    add recursivity to Create Folder if Empty
    * recursivity: if the parent folder doesn't exist, it looks up to 5 levels upwards
    * error message: catches invalid path names

--- BackgroundElementsRandomizer/Assets/CustomAssets/BackgroundElementsRandomizer/Scripts/NonEditor/GeneralOperations.cs
+++ BackgroundElementsRandomizer/Assets/CustomAssets/BackgroundElementsRandomizer/Scripts/NonEditor/GeneralOperations.cs
@@ -9,17 +9,45 @@ namespace Nolanfa
     public static class GeneralOperations
     {
 
-        public static void CreateFolderIfEmpty(string path)
+        public static void CreateFolderIfEmpty(string path, int depth = 0, string origFolder = "", string origPath = "")
         {
+            string errorMessage = "You're trying to create the folder " + origFolder + " at " + origPath + ", but " + origPath + "doesn't exist.";
             if (!AssetDatabase.IsValidFolder(path))
             {
                 int lastSlash = path.LastIndexOf(Path.AltDirectorySeparatorChar);
+                if (lastSlash == -1)
+                {
+                    if(depth == 0)
+                    {
+                        Debug.LogError("Trying to create folder " + path + "; invalid path.");
+                    }
+                    else
+                    {
+                        Debug.LogError(errorMessage);
+                    }
+                    return;
+                }
                 string[] splitPath = new string[2];
                 splitPath[0] = path.Substring(0, lastSlash);
                 splitPath[1] = path.Substring(lastSlash + 1);
                 if (!AssetDatabase.IsValidFolder(splitPath[0]))
                 {
-                    Debug.LogError("You're trying to create the folder " + splitPath[1] + " at " + splitPath[0] + ", but " + splitPath[0] + "doesn't exist.");
+                    if(depth > 5)
+                    {
+                        Debug.LogError(errorMessage);
+                        return;
+                    }
+                    else
+                    {
+                        if (depth == 0)
+                        {
+                            CreateFolderIfEmpty(splitPath[0], depth + 1, splitPath[1], splitPath[0]);
+                        }
+                        else
+                        {
+                            CreateFolderIfEmpty(splitPath[0], depth + 1, origFolder, origPath);
+                        }
+                    }
                 }
                 AssetDatabase.CreateFolder(splitPath[0], splitPath[1]);
             }
